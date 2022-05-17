a7673c5f4568379f105fb9116c4426fe4a5c7aa6
Sarah Lopez <slopez323@gmail.com>
2022-05-17 12:31:44 -0400

    1st assignment

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Users' });
+  // res.send('respond with a resource');
+});
+
+router.get('/myname', function(req, res, next) {
+  res.send('Sarah');
+});
+
+router.get('/myfavoritemovies', function(req, res, next) {
+  res.json(['The Proposal', 'Coda', 'Win a Date with Tad Hamilton']);
 });
 
 module.exports = router;
