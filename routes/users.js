3a2b778b5477a741d64f5d805280e786dd66f857
Sarah Lopez <slopez323@gmail.com>
2022-05-17 12:46:10 -0400

    stretch

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'Users' });
-  // res.send('respond with a resource');
+  res.render('index', { title: 'Users page' });
+  res.send('respond with a resource');
 });
 
 router.get('/myname', function(req, res, next) {
+  res.render('users', { title: 'Name', name: 'Sarah' });
   res.send('Sarah');
 });
 
 router.get('/myfavoritemovies', function(req, res, next) {
+  res.render('users-movies', { title: 'Movies', movies: ['The Proposal', 'Coda', 'Win a Date with Tad Hamilton'] });
   res.json(['The Proposal', 'Coda', 'Win a Date with Tad Hamilton']);
 });
 
