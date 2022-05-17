0e1aff9da6c18a21e89ce2eb136f4e0d17adbb46
Sarah Lopez <slopez323@gmail.com>
2022-05-17 12:52:36 -0400

    render date

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/datetime', function(req, res, next) {
+  res.render('date', { title: 'Date and Time', date: new Date().toString() });
+  res.send(new Date().toString());
+});
+
 module.exports = router;
